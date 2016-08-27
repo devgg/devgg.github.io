347e40c42eb61f2449c5999bd514cde83c685c2f
Florian Gauger <flo.gauger@gmail.com>
2016-08-27 10:50:51 +0200

    Add grid to finance charts

--- finance/charts.js
+++ finance/charts.js
@@ -9,15 +9,11 @@ Quandl.prototype = {
     constructor: Quandl,
 
     setResult: function (data) {
-        var parseDate = d3.time.format("%Y-%m-%d").parse;
+        var parseDate = d3.timeParse("%Y-%m-%d");
         this.formatedResult = data.dataset.data.map(function (d) {
             return {
                 date: parseDate(d[0]),
-                open: +d[1],
-                high: +d[2],
-                low: +d[3],
-                close: +d[4],
-                volume: +d[5]
+                close: +d[4]
             };
         }).reverse();
     }
@@ -30,53 +26,52 @@ function createGraph(data, divId) {
         width = 960 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
+    var parseTime = d3.timeParse("%d-%b-%y");
+
+    var x = d3.scaleTime().range([0, width]);
+
+    var y = d3.scaleLinear().range([height, 0]);
+
+    var line = d3.line()
+        .x(function (d) {
+            return x(d.date);
+        })
+        .y(function (d) {
+            return y(d.close);
+        });
     var svg = d3.select("#" + divId).append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var x = techan.scale.financetime()
-        .range([0, width])
-        .outerPadding(0);
 
-    var y = d3.scale.linear()
-        .range([height, 0]);
+    x.domain(d3.extent(data, function (d) {
+        return d.date;
+    }));
+    y.domain(d3.extent(data, function (d) {
+        return d.close;
+    }));
 
-    var close = techan.plot.close()
-        .xScale(x)
-        .yScale(y);
-
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
-
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
-
-    var accessor = close.accessor();
-
-    x.domain(data.map(accessor.d));
-    y.domain(techan.scale.plot.ohlc(data, accessor).domain());
 
     svg.append("g")
-        .datum(data)
-        .attr("class", "close")
-        .call(close);
-
-    svg.append("g")
-        .attr("class", "x axis")
+        .attr("class", "axis axis--x")
         .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+        .call(d3.axisBottom(x).tickSizeInner(-height));
 
     svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y).tickSizeInner(-width))
         .append("text")
+        .attr("class", "axis-title")
         .attr("transform", "rotate(-90)")
         .attr("y", 6)
         .attr("dy", ".71em")
         .style("text-anchor", "end")
         .text("Price ($)");
+
+    svg.append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", line);
 }
