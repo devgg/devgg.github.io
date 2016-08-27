30d92b9c91f28ae3786a11a2ca9a71ed1d23b7ed
Florian Gauger <flo.gauger@gmail.com>
2016-08-27 11:01:16 +0200

    Add home page with basic social media buttons

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    var github = "https://github.com/devgg";
+    var linkedin = "https://de.linkedin.com/in/floriangauger";
+    var email = "flo.gauger@gmail.com";
+    var resume = "resume.pdf";
+
+    $("#github").click(function () {
+        window.open(github, "_blank");
+    });
+
+    $("#linkedin").click(function () {
+        window.open(linkedin, "_blank");
+    });
+
+    $("#email").click(function () {
+        window.location.href = "mailto:" + email;
+    });
+
+    $("#resume").click(function () {
+        window.open(resume, "_blank");
+    });
+});
