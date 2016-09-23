c63d84b2992448e9d1c7b9a18429dd0b181179be
Florian Gauger <flo.gauger@gmail.com>
2016-09-23 20:38:17 +0200

    Add build dir

--- script.sh
+++ script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-npm install -g uglify-js
 npm install -g clean-css
+npm install -g uglify-js
 
-uglifyjs js/main.js
-cleancss css/main.css
+rsync -av --exclude='css/' --exclude='js/' . build/
+
+cleancss css/main.css -o build/css/main.css
+uglifyjs js/main.js -o build/js/main.js
 
 STR="Hello World!"
 echo $STR
