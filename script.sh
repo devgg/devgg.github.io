f4531d3634cdec283a18c3e7f316025c6661c51a
Florian Gauger <flo.gauger@gmail.com>
2016-09-23 21:18:33 +0200

    Fix bug in git branch creation

--- script.sh
+++ script.sh
@@ -3,6 +3,15 @@
 npm install -g clean-css
 npm install -g uglify-js
 
+mkdir build
+cd build
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+git checkout -b gh-pages || git checkout --orphan gh-pages
+rm -rf build/**/* || exit 0
+
+cd ..
 rsync -av --exclude='css/' \
           --exclude='js/' \
           --exclude='.travis.yml' \
@@ -16,10 +25,6 @@ cleancss css/main.css -o build/css/main.css
 uglifyjs js/main.js -o build/js/main.js
 
 cd build
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
-
-git checkout -b gh-pages
 git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
