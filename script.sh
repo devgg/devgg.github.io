f15a8f820b2733b7b0cf75233c9c1036e6bcb140
Florian Gauger <flo.gauger@gmail.com>
2016-09-23 20:43:24 +0200

    Add exclude dirs to build

--- script.sh
+++ script.sh
@@ -3,8 +3,16 @@
 npm install -g clean-css
 npm install -g uglify-js
 
-rsync -av --exclude='css/' --exclude='js/' . build/
+rsync -av --exclude='css/' \
+          --exclude='js/' \
+          --exclude='.git/' \
+          --exclude='.travis.yml' \
+          --exclude='.gitignore' \
+          --exclude='script.sh' \
+          . build/
 
+mkdir build/css/
+mkdir build/js/
 cleancss css/main.css -o build/css/main.css
 uglifyjs js/main.js -o build/js/main.js
 
