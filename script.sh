cfb3552824e512a0995d449fa78789670aeec788
Florian Gauger <flo.gauger@gmail.com>
2016-09-23 21:13:52 +0200

    Add git commands for github pages branch

--- script.sh
+++ script.sh
@@ -5,15 +5,22 @@ npm install -g uglify-js
 
 rsync -av --exclude='css/' \
           --exclude='js/' \
-          --exclude='.git/' \
           --exclude='.travis.yml' \
           --exclude='.gitignore' \
           --exclude='script.sh' \
           . build/
 
-mkdir build/css/
-mkdir build/js/
+mkdir build/css
+mkdir build/js
 cleancss css/main.css -o build/css/main.css
 uglifyjs js/main.js -o build/js/main.js
 
-ls -LR
+cd build
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+git checkout -b gh-pages
+git add .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+ls -LR build
