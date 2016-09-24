bce4db313289bf2d2a9be0eeb833682415ffdf15
Florian Gauger <flo.gauger@gmail.com>
2016-09-24 10:13:17 +0200

    Change SHA used for commit message

--- build/script.sh
+++ build/script.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-ENCRYPTED_KEY_VAR="encrypted_2f5895d43ae4_key"
-ENCRYPTED_IV_VAR="encrypted_2f5895d43ae4_iv"
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in build/deploy_key.enc -out build/deploy_key -d
 chmod 600 build/deploy_key
 eval `ssh-agent -s`
 ssh-add build/deploy_key
-
-npm install -g clean-css
-npm install -g uglify-js
 
 
 git clone git@github.com:devgg/devgg.git out
@@ -37,6 +34,9 @@ rsync -av --exclude='.git' \
           --exclude='deploy_key.enc' \
           . out/
 
+
+npm install -g clean-css
+npm install -g uglify-js
 mkdir out/css
 mkdir out/js
 cleancss css/main.css -o out/css/main.css
@@ -44,7 +44,7 @@ uglifyjs js/main.js -o out/js/main.js
 
 cd out
 git add -A
-SHA=`git rev-parse --verify HEAD`
+SHA=`git rev-parse origin/master`
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 git remote set-url origin git@github.com:devgg/devgg.git
 git push origin gh-pages
